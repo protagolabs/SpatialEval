b3a90921319174ea5d6d6078116f345c937537f0
heya5 <yanhe.ian@outlook.com>
2024-05-21 14:19:18 +0000

    update

--- run_exp01.sh
+++ run_exp01.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 set -e
+export PYTHONPATH=.:$PYTHONPATH
+
 # MODELS="Qwen/CodeQwen1.5-7B-Chat Qwen/Qwen1.5-7B-Chat"
 # MODELS="internlm/internlm2-chat-7b 01-ai/Yi-1.5-9B-Chat mistralai/Mixtral-8x7B-Instruct-v0.1"
 # MODELS="deepseek-ai/DeepSeek-V2-Lite-Chat 01-ai/Yi-1.5-34B-Chat"
@@ -9,7 +11,10 @@ MODELS=(
     # "meta-llama/Llama-2-13b-chat-hf"
     # "internlm/internlm2-chat-1_8b"
     # "internlm/internlm2-chat-20b"
+    "deepseek-ai/DeepSeek-V2-Lite-Chat"
     "meta-llama/Meta-Llama-3-70B-Instruct"
+    "deepseek-ai/DeepSeek-V2-Chat"
+    "mistralai/Mixtral-8x7B-Instruct-v0.1"
 )
 
 for MODEL in $MODELS
